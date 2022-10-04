2d367bbabe1876c76bf17d28638a2196b030c689
luis-moguea <luis.mogueadp@gmail.com>
2022-10-04 18:15:52 -0500

    uploading new project

---
+++ index.js
@@ -0,0 +1,86 @@
+const DATA = [
+
+{
+id: 1,
+profile_picture: pending,
+location: pending,
+main_image: pending,
+like_icon: pending,
+comms_icon: pending,
+sharing_icon: pending,
+favorite_icon: pending,
+likes_count: pending,
+ramdom_comment: [
+    jkdhljdshfvjhsdlfvldfjl, 
+    jdkjdkfnndfnvdnfmnfd,
+    kjadhljshldjhfjsdfhfsd,
+    sjdhljsdkjsldjlkflsdjfsrf,
+    sjdhclkjshdljcslkjdkjsbdjcjsb
+]
+},
+
+{
+    id: 1,
+    profile_picture: pending,
+    location: pending,
+    main_image: pending,
+    like_icon: pending,
+    comms_icon: pending,
+    sharing_icon: pending,
+    favorite_icon: pending,
+    likes_count: pending,
+    ramdom_comment: [
+        jkdhljdshfvjhsdlfvldfjl, 
+        jdkjdkfnndfnvdnfmnfd,
+        kjadhljshldjhfjsdfhfsd,
+        sjdhljsdkjsldjlkflsdjfsrf,
+        sjdhclkjshdljcslkjdkjsbdjcjsb
+    ]
+
+},
+
+{
+
+id: 1,
+profile_picture: pending,
+location: pending,
+main_image: pending,
+like_icon: pending,
+comms_icon: pending,
+sharing_icon: pending,
+favorite_icon: pending,
+likes_count: pending,
+ramdom_comment: [
+    jkdhljdshfvjhsdlfvldfjl, 
+    jdkjdkfnndfnvdnfmnfd,
+    kjadhljshldjhfjsdfhfsd,
+    sjdhljsdkjsldjlkflsdjfsrf,
+    sjdhclkjshdljcslkjdkjsbdjcjsb
+]
+
+},
+
+{
+
+id: 1,
+profile_picture: pending,
+location: pending,
+main_image: pending,
+like_icon: pending,
+comms_icon: pending,
+sharing_icon: pending,
+favorite_icon: pending,
+likes_count: pending,
+ramdom_comment: [
+    jkdhljdshfvjhsdlfvldfjl, 
+    jdkjdkfnndfnvdnfmnfd,
+    kjadhljshldjhfjsdfhfsd,
+    sjdhljsdkjsldjlkflsdjfsrf,
+    sjdhclkjshdljcslkjdkjsbdjcjsb
+]
+}
+
+]
+
+
+const container = document.querySelector("#item-list")
