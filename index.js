21f3098b827e84f44309af770c81667059248a17
luis-moguea <luis.mogueadp@gmail.com>
2022-10-09 09:40:41 -0500

    uploading js updates

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const DATA = [
-
 {
-
     id: "Cat.2004",
-    profile_picture: "/images/cat-1.jpg",
+    profile_picture: "https://source.unsplash.com/random/200x200?sig=1",
     ellipsis_icon: "/images/svg-ellipsis.png",
     location: "Huston, Texas - United States",
     main_image: "/images/cat-1.jpg",
@@ -20,14 +18,12 @@ const DATA = [
         "Check your DM honey jeje",
     ]
 },
-
 {
-
     id: "Cat.132",
-    profile_picture: "/images/cat-2.jpg",
+    profile_picture: "https://source.unsplash.com/random/200x200?sig=2",
     ellipsis_icon: "/images/svg-ellipsis.png",
     location: "Arlington, Texas - United States",
-    main_image: "/images/cat-2.jpg",
+    main_image: "https://source.unsplash.com/random/800x800?sig=3",
     like_icon: "/images/svg-heart-black.png",
     like_icon_two: "/images/svg-heart-white.png",
     comms_icon: "/images/svg-comm.png",
@@ -41,9 +37,7 @@ const DATA = [
         "Check your DM honey jeje",
     ]
 },
-
 {
-
     id: "Cat.44",
     profile_picture: "/images/cat-3.jpg",
     ellipsis_icon: "/images/svg-ellipsis.png",
@@ -62,9 +56,7 @@ const DATA = [
         "Check your DM honey jeje",
     ]
 },
-
 {
-
     id: "Cat.689",
     profile_picture: "/images/cat-4.jpg",
     ellipsis_icon: "/images/svg-ellipsis.png",
@@ -83,40 +75,36 @@ const DATA = [
         "Check your DM honey jeje",
     ]
 }
-
-
-
 ]
-
 
 const container = document.querySelector("#item-list")
 
 DATA.map(el => {
-    const element = `
+    const element = /* html */`
     <div class="hero__main">
-        <div class= "hero__main--profile">
-            <div class= "hero__main--profile--img">
+        <div class="hero__main--profile">
+            <div class="hero__main--profile--img">
                 <img src="${el.profile_picture}" alt="">
             </div>
-            <div class= "hero__main--profile--id">
+            <div class="hero__main--profile--id">
                 <p>${el.id}</p>
                 <img src="${el.ellipsis_icon}" alt="">
             </div>
         </div>
-        <div class= "hero__main--image">
+        <div class="hero__main--image">
             <img src="${el.main_image}" alt="">
         </div>
-        <div class= "hero__main--icons">
-            <div class= "hero__main--icons--1">
+        <div class="hero__main--icons">
+            <div class="hero__main--icons--1">
                 <img src="${el.like_icon}" alt="">
                 <img src="${el.comms_icon}" alt="">
                 <img src="${el.sharing_icon}" alt="">
             </div>
-            <div class= "hero__main--icons--2">
+            <div class="hero__main--icons--2">
                 <img src="${el.favorite_icon}" alt="">
             </div>
         </div>
-        <div class= "hero__main--comms">
+        <div class="hero__main--comms">
             <p>${el.likes_count}</p>
             <p>${el.ramdom_comment}</p>
         </div>
